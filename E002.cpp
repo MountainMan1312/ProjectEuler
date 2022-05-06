d6f52d79082fe7f101ffc33543153735d5ffd15f
Tristan Williams <tristan@profabky.com>
2022-05-06 01:55:25 -0400

    Completed Problem 002
    - Modified main.cpp
    - Modified E002.cpp

--- E002.cpp
+++ E002.cpp
@@ -1,11 +1,27 @@
 /*E002.cpp
-* 
+* Returns the sum of even fibonacci terms under a maxValue
 */
 
 #include <iostream>
 #include "ProjectEuler.h"
 
-int E002()
+int E002(int maxValue)
 {
-	return 0; //temporary placeholder
+	int fibX = 1;
+	int fibY = 2;
+	int fibNext = 0;
+	int sumOfEvens = 0;
+
+	while (fibY <= maxValue)
+	{
+		if (fibY % 2 == 0) { sumOfEvens += fibY; } //add current number to sum if it's even
+
+		{ //Increment fibonacci sequence
+			fibNext = fibX + fibY;
+			fibX = fibY;
+			fibY = fibNext;
+		}
+	}
+
+	return sumOfEvens;
 }
--- main.cpp
+++ main.cpp
@@ -3,11 +3,6 @@
 * In this project, I aim to write a function for every Euler problem.
 * I intend for each function to be able to take any values, and test
 * for the project's values here in the main.
-* 
-* The program has a top level interface with options:
-* 1. Output results to all problems
-* 2. Specify a problem (default values)
-* 3. Specify a problem (custom values)
 */
 
 #include <iostream>
@@ -15,7 +10,10 @@
 
 int main()
 {
-	std::cout << "1: " << E001(3, 5, 1000); // Problem 001
+	std::cout << "001: " << E001(3, 5, 1000) << "\n"; //Problem 001 - Sum of multiples of 3 or 5 under 1000
+	std::cout << "002: " << E002(4000000) << "\n";    //Problem 002 - Sum of even-valued Fibonacci terms under 4 million
 
-	//TODO: Make the menu thing
+	return 0;
 }
+
+//TODO: Add function to test for correct result
