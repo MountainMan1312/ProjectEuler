3c0d9c2bafa771c64c16d6652d1fd670745ddd19
Tristan Williams <tristan@profabky.com>
2022-05-08 00:09:21 -0400

    Add result-testing functionality
    - add testResult() to main.cpp
    - rework calls to submissions in main() to use testResult()

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ProjectEuler
 
 This project contains my solutions to the problems presented by [The Euler Project](https://projecteuler.net/).
-
-main.cpp is the entry point
--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 * It is recommended to avoid looking at other people's solutions until after you've tried it yourself.
 */
 
-
 /*EXPANSIONS TO PROJECT EULER TO MAKE IT MORE INTERESTING
 * So far, here's what I've done:
 * - Brought all the problems into a single project
@@ -29,12 +28,33 @@
 
 #include <iostream>
 #include "ProjectEuler.h"
+#define string std::string //all my homies hate std::string
+
+void testResult(string problem, int function, int answer)
+{
+	std::cout << problem << " | ";
+
+	{ //Test for correctness and print to console
+		if (function == answer) { std::cout << "CORRECT   | "; }
+		else { std::cout << "INCORRECT | "; }
+	}
+
+	std::cout << function << "\n";
+
+}
 
 int main()
-{
-	std::cout << "001: " << E001(3, 5, 1000) << "\n";   //Problem 001 - Sum of multiples of 3 or 5 under 1000
-	std::cout << "002: " << E002(4000000) << "\n";      //Problem 002 - Sum of even-valued Fibonacci terms under 4 million
-	std::cout << "003: " << E003(600851475143) << "\n"; //TODO: Problem 003 - Largest Prime Factor of 600851475143
+{	
+	//Keep these just in case, for now
+	//std::cout << "001: " << E001(3, 5, 1000) << "\n";   //Problem 001 - Sum of multiples of 3 or 5 under 1000
+	//std::cout << "002: " << E002(4000000) << "\n";      //Problem 002 - Sum of even Fibonacci terms under 4 million
+	//std::cout << "003: " << E003(600851475143) << "\n"; //TODO: Problem 003 - Largest Prime Factor of 600851475143
+
+	//use this: testResult("", E(), );
+
+	testResult("001", E001(3, 5, 1000), 233168); //Problem 001 - Sum of multiples of 3 or 5 under 1000
+	testResult("002", E002(4000000), 4613732);          //Problem 002 - Sum of even Fibonacci terms under 4 million
+	testResult("003", E003(600851475143), 6857);     //TODO: Problem 003 - Largest Prime Factor of 600851475143
 
 	return 0;
 }
