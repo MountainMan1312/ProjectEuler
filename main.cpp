3fbb890af3fb20b190a3ed2cabe3894330396cdd
Mountain Man <43313373+MountainMan1312@users.noreply.github.com>
2022-05-08 23:14:26 -0400

    Need help with function pointers

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@
 * So far, here's what I've done:
 * - Brought all the problems into a single project
 *	- Each problem gets its own file, in the format EXXX, where XXX = problem number
-* 
+*
 * Future plans:
 * TODO: Add run-time testing
 *	- Test to see if submission runs within the suggested time (1 minute)
-* 
+*
 * TODO: Add multiple-submission functionality
-* 
+*
 * TODO: Add multi-threading
 *	- Each problem / submission gets its own thread (I guess? I don't know anything yet)
-* 
+*
 * TODO: Add reporting
 *	- Generate a report of results, run-times, etc.
 */
@@ -27,26 +27,54 @@
 #include "ProjectEuler.h"
 #define string std::string //all my homies hate std::string
 
-void testResult(string problem, int function, int answer)
+int(*currentFuncPtr)();
+
+//TEST RESULT AND EXECUTION TIME
+void testResult(string problem, int* currentFuncPtr, int answer)
 {
 	std::cout << problem << " | ";
 
-	{ //Test for correctness and print to console
-		if (function == answer) { std::cout << "CORRECT   | "; }
-		else { std::cout << "INCORRECT | "; }
-	}
-
-	std::cout << function << "\n";
+	//get start time
+	int attempt = *currentFuncPtr;         // DOES THIS ACTUALLY RUN THE FUNCTION HERE?
+	//get end time                         // OR DOES IT RUN IN main() LIKE I DON'T WANT IT TO?
+	//TODO: calculate the time stuff
 
 }
 
 int main()
 {
-	//use this: testResult("", E(), );
 
-	testResult("001", E001(3, 5, 1000), 233168); //Problem 001 - Sum of multiples of 3 or 5 under 1000
-	testResult("002", E002(4000000), 4613732);   //Problem 002 - Sum of even Fibonacci terms under 4 million
-	testResult("003", E003(600851475143), 6857); //TODO: Problem 003 - Largest Prime Factor of 600851475143
+	//In each code block, I need to assign the current problem function to currentFuncPtr.
+	//Then I need to send that to testResult, with the arguments, without it actually running.
+	//I need it to run in testResult because that's where execution time is being measured.
+
+	{ //PROBLEM 001 - Sum of multiples of 3 or 5 under 1000
+		int(*currentFuncPtr)(int, int, int) = &E001(3, 5, 1000); //HEY LOOK AT ME - THIS IS WHERE THE BAD THING HAPPENS!!!
+		testResult("001", currentFuncPtr, 233168);
+	}
+
+	//The code above produces the follwing errors:
+	// - expression must be an lvalue or a functon designator
+	// - argument of type "int(*)(int, int, int)" is incompatible with parameter of type "int*"
+
+
+
+
+
+	// IGNORE EVERYTHING BELOW THIS LINE IT DOESN'T MATTER
+
+
+
+
+
+	//use this: testResult("", E(), );
+	//testResult("001", E001(3, 5, 1000), 233168); //Problem 001 - Sum of multiples of 3 or 5 under 1000
+	//testResult("002", E002(4000000), 4613732);   //Problem 002 - Sum of even Fibonacci terms under 4 million
+	//testResult("003", E003(600851475143), 6857); //TODO: Problem 003 - Largest Prime Factor of 600851475143
+
+
+	//std::cout << "PROGRAM COMPLETE - PRESS KEY TO END\n";
+	//std::cin.get();
 
 	return 0;
 }
