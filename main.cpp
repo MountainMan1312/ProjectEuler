23d65ba3e3b379c65200ba1ab99090d43dbfb4bf
Mountain Man <43313373+MountainMan1312@users.noreply.github.com>
2022-05-10 21:09:32 -0400

    Add execution-time testing
    - Don't remember what I did

--- main.cpp
+++ main.cpp
@@ -24,57 +24,49 @@
 */
 
 #include <iostream>
+#include <chrono>
+#include <iomanip>
 #include "ProjectEuler.h"
 #define string std::string //all my homies hate std::string
 
-int(*currentFuncPtr)();
-
-//TEST RESULT AND EXECUTION TIME
-void testResult(string problem, int* currentFuncPtr, int answer)
+//TEST RESULTS AND EXECUTION TIME
+void testResult(string problem, int testFunc, int answer)
 {
 	std::cout << problem << " | ";
 
-	//get start time
-	int attempt = *currentFuncPtr;         // DOES THIS ACTUALLY RUN THE FUNCTION HERE?
-	//get end time                         // OR DOES IT RUN IN main() LIKE I DON'T WANT IT TO?
-	//TODO: calculate the time stuff
+	auto startTime = std::chrono::system_clock::now(); //Start execution time test
+	auto result = testFunc;                            //Run submitted function
+	auto endTime = std::chrono::system_clock::now();   //End execution time test
+	
+	//Calculate execution time
+	std::chrono::duration<double> elapsed_seconds = endTime - startTime;
+	std::cout << std::setprecision(8) << std::fixed << elapsed_seconds.count() << " s" << " | ";
 
+	//Correct or incorrect? Print results.
+	if (result == answer) { std::cout << "CORRECT   | " << result << "\n"; }
+	else { std::cout << "INCORRECT | " << result << "\n"; }
 }
 
 int main()
 {
-
-	//In each code block, I need to assign the current problem function to currentFuncPtr.
-	//Then I need to send that to testResult, with the arguments, without it actually running.
-	//I need it to run in testResult because that's where execution time is being measured.
-
 	{ //PROBLEM 001 - Sum of multiples of 3 or 5 under 1000
-		int(*currentFuncPtr)(int, int, int) = &E001(3, 5, 1000); //HEY LOOK AT ME - THIS IS WHERE THE BAD THING HAPPENS!!!
-		testResult("001", currentFuncPtr, 233168);
+		auto func = +[]() { return E001(3, 5, 1000); };
+		testResult("001", func(), 233168);
 	}
 
-	//The code above produces the follwing errors:
-	// - expression must be an lvalue or a functon designator
-	// - argument of type "int(*)(int, int, int)" is incompatible with parameter of type "int*"
+	{ //PROBLEM 002 - Sum of even Fibonacci terms under 4 million
+		auto func = +[]() { return E002(4000000); };
+		testResult("002", func(), 4613732);
+	}
+	
+	{ //PROBLEM 003 - Largest Prime Factor of 600851475143
+		auto func = +[]() { return E003(600851475143); };
+		testResult("003", func(), 6857);
+	}
 
-
-
-
-
-	// IGNORE EVERYTHING BELOW THIS LINE IT DOESN'T MATTER
-
-
-
-
-
-	//use this: testResult("", E(), );
-	//testResult("001", E001(3, 5, 1000), 233168); //Problem 001 - Sum of multiples of 3 or 5 under 1000
-	//testResult("002", E002(4000000), 4613732);   //Problem 002 - Sum of even Fibonacci terms under 4 million
-	//testResult("003", E003(600851475143), 6857); //TODO: Problem 003 - Largest Prime Factor of 600851475143
-
-
-	//std::cout << "PROGRAM COMPLETE - PRESS KEY TO END\n";
-	//std::cin.get();
+	//Really this is just here to keep all that crap from showing at the end
+	std::cout << "TESTS COMPLETE\n";
+	std::cin.get();
 
 	return 0;
 }
